0fb71f45d73da68ab3ce392806941eb4a657e965
Kola.In <142318756+Kola9INE@users.noreply.github.com>
2023-11-29 13:14:16 +0100

    Update vgsales.py

--- vgsales.py
+++ vgsales.py
@@ -5,7 +5,7 @@ from urllib.error import URLError
 
 @st.cache_data
 def get_data():
-    path_ = "datasets/vgsales.csv"
+    path_ = "vgsales.csv"
     df = pd.read_csv(path_)
     # Dropping null values from Year and Publisher
     df.dropna(inplace = True)
